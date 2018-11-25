2588b7f8437e52b2eaa0fa7664e08830835d1b3e
willrapha <willraphaspace@gmail.com>
2018-11-25 13:33:21 -0200

    config cookies identity

--- OverviewIdentity/Id.Overview.Mvc/Startup.cs
+++ OverviewIdentity/Id.Overview.Mvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Id.Overview.Mvc.Data;
 using Id.Overview.Mvc.Models;
 using Id.Overview.Mvc.Services;
+using Microsoft.AspNetCore.Http;
 
 namespace Id.Overview.Mvc
 {
@@ -50,7 +51,7 @@ namespace Id.Overview.Mvc
                     options.SignIn.RequireConfirmedEmail = false; // Determina para o usuario realizar o signIn no sistema ele deve ter um email confirmado
                     options.SignIn.RequireConfirmedPhoneNumber = false; // Determina para o usuario realizar o signIn no sistema o usuario deve ter confirmado um numero de telefone
 
-                    // Tokens Options
+                    // Tokens Options // Comentado para não sobrescrever valor default
                     // options.Tokens.AuthenticatorTokenProvider // Utilizado para gerar valores para o two factory authentication
                     // options.Tokens.ChangeEmailTokenProvider // Obtem ou definine o ChangeEmailTokenProvider usado para gerar tokens do email de confirmação do processo de alteração de email do usuario
                     // options.Tokens.ChangePhoneNumberTokenProvider // Obtem ou definine o ChangePhoneNumberTokenProvider usado para gerar tokens para o telefone de confirmação do processo de alteração do telefone do usuario
@@ -63,6 +64,32 @@ namespace Id.Overview.Mvc
                 })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            // Cookies Identity
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Alguns parametros foram comentados para manter o padrao default dos dados do Identity
+                options.AccessDeniedPath = "/Account/AcessDenied"; // Caminho para o handle responsavel por tratar o erro de status 403
+                // options.ClaimsIssuer = ""; // Obtem ou define um Issuer que sera utilizado para criação de qualquer claim
+                // options.Cookie.Domain = ""; // Define o dominio ao qual o cookie que sera criado pertence
+                // options.Cookie.Expiration = ""; // Obtem ou define o tempo de vida do cookie http
+                options.Cookie.HttpOnly = true; // Define se a propriedade pode ou não ser acessada pelo lado do Cliente
+                options.Cookie.Name = ".AspNetCore.Cookies"; // Nome do cookie
+                options.Cookie.Path = ""; // Caminho do cookie
+                options.Cookie.SameSite = SameSiteMode.Lax; // Define uma propriedade do cookie SameSite, SameSite são cookies que não devem ser anexo as solicitações pro side - Strict cookies nao são enviados em navegações de nivel superior - Lax cookies são enviados para navegações de nivel superior 
+                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // Configuração das policy
+                // options.CookieManager = ; // Define o componente que sera utilizado para obter os cookies nos request ou dos disponse
+                // options.DataProtectionProvider = ; // Provider usado pelo CookieAuthenticationHandler para a proteção dos dados
+                // options.Events = ; // Handler ou manipulador responsavel por chamar os metodos do provider que dara controle da aplicação em certos pontos que ocorrem processamento
+                // options.EventsType = ; // Servico que sera responsavel por obter a instacia dos eventos
+                options.ExpireTimeSpan = TimeSpan.FromDays(14); // Controla o tempo do ticket de autenticacao permanecera valido a partir do momento que foi criado
+                options.LoginPath = "/Account/Login"; // Caminho onde o usuario ira logar caso determinado controler precise de autorização
+                options.LogoutPath = "/Accout/Logout"; // Caminho onde o usuario ira se deslogar 
+                options.ReturnUrlParameter = "ReturnUrl"; // Nome do parametro que recebera a url que usuario devera ser redireciado apos realizar o login
+                // options.SessionStore = ; // Define um container opcional ao qual armazenara a identidade do usuario que fara as requisicoes
+                options.SlidingExpiration = true; // Quando habilitado um novo cookie sera criado com uma nova hora de expiração quando o atual tiver passada da metade do tempo de expiração
+                // options.TicketDataFormat = ; // Utilizado para proteger e desproteger a identidade e outras propriedades armazenadas no valor do cookie
+            });
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
